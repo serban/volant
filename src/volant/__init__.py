2a5dd8cccd4cbd8ca85e44d5c65ae36454308360
Serban Giuroiu <giuroiu@gmail.com>
2025-06-08 23:36:33 -0700

    Add heading()

--- src/volant/__init__.py
+++ src/volant/__init__.py
@@ -1,3 +1,14 @@
 def separator() -> None:
   """Print a nice horizontal line."""
   print(f'  {"─" * 76}  ')
+
+
+def heading(s: str) -> None:
+  """Print a nice box around some text."""
+  pad = max(len(s), 76)
+  line = '─' * pad
+  text = f'{s:{pad}}'
+
+  print(f'╭─{line}─╮')
+  print(f'│ {text} │')
+  print(f'╰─{line}─╯')
--- tests/test_volant.py
+++ tests/test_volant.py
@@ -6,6 +6,18 @@ import volant
 
 kSeparator = '  ────────────────────────────────────────────────────────────────────────────  \n'
 
+kHeadingShort = """
+╭──────────────────────────────────────────────────────────────────────────────╮
+│ Extra! Extra! Read all about it!                                             │
+╰──────────────────────────────────────────────────────────────────────────────╯
+""".lstrip()
+
+kHeadingLong = """
+╭─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────╮
+│ Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. │
+╰─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────╯
+""".lstrip()
+
 
 class VolantTest(unittest.TestCase):
   maxDiff: int | None = None
@@ -15,3 +27,18 @@ class VolantTest(unittest.TestCase):
       with contextlib.redirect_stdout(buffer):
         volant.separator()
       self.assertEqual(kSeparator, buffer.getvalue())
+
+  def test_heading(self):
+    for sub, out, arg in [
+      ('extra', kHeadingShort, 'Extra! Extra! Read all about it!'),
+      (
+        'lorem',
+        kHeadingLong,
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      ),
+    ]:
+      with self.subTest(sub):
+        with io.StringIO() as buffer:
+          with contextlib.redirect_stdout(buffer):
+            volant.heading(arg)
+          self.assertEqual(out, buffer.getvalue())
